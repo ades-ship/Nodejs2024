ba01ff3546f43caed5948c5c6204bb9082b48760
adesh sengar <adeshsengar_20021763.cse@geu.ac.in>
2024-04-11 02:21:49 +0530

    cover basic modules in nodejs

---
+++ test.js
@@ -0,0 +1,21 @@
+const calculator=require('./calculator')
+const os=require('os')
+const path=require('path')
+const fs=require('fs')
+// console.log(calculator.addtion(2,4))
+// console.log(calculator.substraction(2,4))
+// console.log(calculator.mul(2,4))
+// console.log(calculator.div(2,4))
+// console.log(os.platform());
+// console.log(os.arch())
+// console.log(os.networkInterfaces())
+// console.log(os.cpus())
+// console.log(os.totalmem())
+// console.log(path.basename('path:C:\Users\Administrator\Desktop\Scalar Nodejs\f1.txt'))
+// let ext=path.extname('path:C:\Users\Administrator\Desktop\Scalar Nodejs\f1.txt')
+// console.log(ext)
+
+// fs.appendFileSync('path:f2.txt',"data:this is update file")
+// fs.appendFileSync(path:'f2.txt')
+
+console.log("mesaage: updated file");
