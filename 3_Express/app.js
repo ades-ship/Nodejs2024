c5a02b1dacc18fdc8c4cb9e22667e182cd5ede00
adesh sengar <adeshsengar_20021763.cse@geu.ac.in>
2024-04-14 01:45:04 +0530

    add the express sever to the application

--- 3_Express/app.js
+++ 3_Express/app.js
@@ -1,3 +1,5 @@
+// run the express nodemon server ->   npm run serve
+
 const express=require('express')
 
 const app=express();
@@ -9,6 +11,12 @@ app.get('/about',(req,res)=>{
     res.send('create the impact');
 })
 
+app.get('/contact',(req,res)=>{
+    res.send('contact adesh@156gmail.com')
+})
+
+const port=process.env.PORT || 3000
+
 app.listen(3000,()=>{
-console.log('port is running on 3000')
+console.log(`port is running on ${port}`);
 })
